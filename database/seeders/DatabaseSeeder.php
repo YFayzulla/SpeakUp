77695d8302ca2ed59c36973dd807efc6aee63b65
YoldashevFayzulla <yoldashevfayzulla0@gmail.com>
2023-05-28 15:17:10 +0500

    generaly

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
+         \App\Models\User::factory()->create();
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);

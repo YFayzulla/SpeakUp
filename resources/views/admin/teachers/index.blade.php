ab429eb060f997e3c38a7109ac2b054f21cd6970
YoldashevFayzulla <yoldashevfayzulla0@gmail.com>
2023-06-01 22:30:05 +0500

    update created

--- resources/views/admin/teachers/index.blade.php
+++ resources/views/admin/teachers/index.blade.php
@@ -1,8 +1,5 @@
 @extends('layouts.admin')
 @section('content')
-
-    {{--@dd($users)--}}
-
     <div class="p-4 m-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg ">
         <a href="{{route('dashboard.create')}}" type="button" class="btn-outline-success btn m-2"> new</a>
             <table class="table table-hover">
@@ -26,7 +23,7 @@
                     <th>{{$user->desc}}</th>
                     <th><img src="Photo/{{$user->image}}" alt="" width="70px"></th>
                     <th class="text-center">
-                        <a href="#" type="button" class="btn-outline-warning btn m-2">edit</a>
+                        <a href="{{route('dashboard.edit',$user->id)}}" class="btn-outline-warning btn m-2">edit</a>
                         <button href="#" type="button" class="btn-outline-danger btn m-2">del</button>
                     </th>
                 </tr>

1438d106010eb8ca8d2e9591281a6a3cf7d86084
YoldashevFayzulla <yoldashevfayzulla0@gmail.com>
2023-06-01 08:51:10 +0500

    js

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -286,7 +286,7 @@
 <script src="{{asset('../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js')}}"></script>
 <script src="{{asset('../assets/vendor/js/menu.js')}}"></script>
 <script src="{{asset('../assets/js/main.js')}}"></script>
-<script src="{{asset('../assets/vendor/js/bootstrap.js')}}"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 
 </body>
 </html>

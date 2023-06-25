dbabcd909408552676b8f7095692f911d3dafc7a
YoldashevFayzulla <yoldashevfayzulla0@gmail.com>
2023-06-25 18:44:36 +0500

    commit

---
+++ database/migrations/2023_06_23_151541_create_students_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('tel');
+            $table->string('parents_tel')->nullable();
+            $table->string('payment');
+            $table->string('summa')->nullable();
+            $table->string('group_id')->nullable();
+            $table->string('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};

6d4be9dc02feace77faaa05d32aeb73a10956167
YFayzulla <yoldashevfayzulla0@gmail.com>
2025-02-11 20:59:26 +0500

    change

--- resources/views/assessment/main.blade.php
+++ resources/views/assessment/main.blade.php
@@ -1,63 +1,66 @@
 @extends('template.master')
 @section('content')
-
-    <div class="container-xxl flex-grow-1 container-p-y">
+    <div class="p-4 m-4 sm:p-8 bg-white shadow sm:rounded-lg ">
         <div class="row">
-            <!-- First Table -->
-            <div class="col-md-6 mt-2">
-                <div class="card">
-                    <h5 class="card-header">Top 5 Students</h5>
-                    <div class="table-responsive text-nowrap">
-                        <table class="table">
-                            <thead>
+            <div class="card">
+                <h5 class="card-header">Active Students</h5>
+                <div class="table-responsive text-nowrap">
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <td>Name</td>
+                            <td>Group</td>
+                            <td>Mark</td>
+                        </tr>
+                        </thead>
+                        <tbody class="table-border-bottom-0">
+                        @foreach($topStudents as $topStudent)
                             <tr>
-                                <td>Name</td>
-                                <td>Group</td>
-                                <td>Mark</td>
+                                <td>{{ $topStudent->student->name }}</td>
+                                <td>{{ $topStudent->group }}</td>
+                                <td>{{ $topStudent->get_mark }}</td>
                             </tr>
-                            </thead>
-                            <tbody class="table-border-bottom-0">
-                            @foreach($topStudents as $topStudent)
-                                <tr>
-                                    <td>{{ $topStudent->student->name }}</td>
-                                    <td>{{ $topStudent->group }}</td>
-                                    <td>{{ $topStudent->get_mark }}</td>
-                                </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                    </div>
+                        @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
+        </div>
 
-            <!-- Second Table -->
-            <div class="col-md-6 mt-2">
-                <div class="card">
-                    <h5 class="card-header">Data List</h5>
-                    <div class="table-responsive text-nowrap">
-                        <table class="table">
-                            <thead>
+    </div>
+
+
+    <!-- Second Table -->
+
+    <div class="p-4 m-4 sm:p-8 bg-white shadow sm:rounded-lg ">
+        <div class="row">
+            <div class="card">
+                <h5 class="card-header">Test List</h5>
+                <div class="table-responsive text-nowrap">
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <td>Test Name</td>
+                            <td>Data</td>
+                            <td>show</td>
+                        </tr>
+                        </thead>
+                        <tbody class="table-border-bottom-0">
+
+                        @foreach($data as $item)
                             <tr>
-                                <td>Name</td>
-                                <td>Data</td>
+                                <td>{{$item->name}}</td>
+                                <td>{{$item->created_at->format('m-m-Y')}}</td>
+                                <td>
+                                    <a href="{{route('test.show', $item->id)}}" class="btn btn-info"><i
+                                                class="bx bx-show-alt"></i></a>
+                                </td>
                             </tr>
-                            </thead>
-                            <tbody class="table-border-bottom-0">
-                            @foreach($data as $item)
-                                <tr>
-                                    <td>
-                                        <b><a href="{{route('test.show',$item->id)}}"
-                                              class="mb-0 m-2 text-secondary">{{ $item->name }}</a></b>
-                                    </td>
-                                    <td>
-                                        <div class="user-progress d-flex align-items-center gap-1">
-                                            <h6 class="mb-0">{{ $item->created_at->format('d-m-y') }}</h6>
-                                        </div>
-                                    </td>
-                                </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
+                        @endforeach
+                        </tbody>
+                    </table>
+                    <div class="card-footer">
+                        {{ $data->links('pagination::bootstrap-5') }}
                     </div>
                 </div>
             </div>

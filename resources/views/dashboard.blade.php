8b536188eb12cf1a384b49983becd22638dd1cbf
YFayzulla <yoldashevfayzulla0@gmail.com>
2025-12-23 12:32:23 +0500

    test

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -68,7 +68,7 @@
                                             <td>
                                                 <div class="d-flex align-items-center">
                                                     <div class="avatar avatar-sm me-3">
-                                                        <img src="{{ 'storage/'.$teacher->photo}}"
+                                                        <img src="{{asset('storage/'.$teacher->photo)}}"
                                                              alt="Avatar" class="rounded-circle">
                                                     </div>
 
